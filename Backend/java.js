fe6840d74e49c1174d9d7564fb249250d8f0a838
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-02 11:23:21 -0500

    small changes made work on adding in cdn and more

--- Backend/java.js
+++ Backend/java.js
@@ -5,3 +5,15 @@ function grabLocalStroage(){
         $(`#text${key}`).text(data);
     }
 }
+
+$(document).ready(function(){
+
+    for(let i = 0; i < searchHistory.length; i++){
+
+    var searchBar = $(`<div class="" id="searchBar"><textarea id="" class="" placeholfer="Search city here"></textarea>`);
+
+    searchHistory.append(seachBar);
+    $("#container").append(searchHistory);
+    }
+});
+
