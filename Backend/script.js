60492fb4719b274e7d0b56baaa27be6383335817
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-03 13:54:23 -0500

    worked on the front end slightly worked on saving data to the local

--- Backend/script.js
+++ Backend/script.js
@@ -8,13 +8,22 @@ function grabLocalStroage(){
 
 $(document).ready(function(){
 
-    for(let i = 0; i < 4; i++){
-
-    var cityLookUp = $(`<div class="col"`)
+    var cityLookUp = $(`<div class="col">`);
     var searchBar = $(`<div class="search" id="searchBar"><textarea id="cityInput" class="userInput" placeholder="Search city here"></textarea>`);
+    var btn = $(`<div class="btn" id="save"><button class="saveBtn"></button>`);
 
     cityLookUp.append(searchBar);
+    cityLookUp.append(btn)
     $(".container").append(cityLookUp);
-    }
+    
 });
+
+     var saveBtn = $('.saveBtn');
+     saveBtn.on('click',function(){
+        let btnClicked = $(this).attr('id');
+        let savedText = $(this).parent().siblings().children('.userInput').val();
+        localStorage.setItem(btnClicked, savedText);
+     })
+
+
 
