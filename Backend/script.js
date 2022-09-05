9e762bdb5a3b9db43e8676e3ac3cd627631d78cb
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-05 18:57:33 -0500

    worked on backend slightly

---
+++ Backend/script.js
@@ -0,0 +1,27 @@
+var city = "";
+
+var searchCity = $("#citySearch");
+var searchBtn = $("#search-button");
+var searchClear = $("#clearSearch");
+var currrentCity = $("#currentCity");
+var currentTemp = $("#temp");
+var curretnWind = $("#wind");
+var currentHumidity  = $("#humidity");
+var currentUv = $("#uvIndex");
+var pastCity = [];
+//Checks to see if the city has previously been searched
+function find(p){
+    for(var i=0; i<pastCity.lengeth; i++) {
+        if(p.toUpperCase() === pastCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+//set up thr API key used to find data
+var APIkey = "104b3d87a3f27b63c86227e77149ab4c"
+
+//function to display the data for city input by user
+function showCurrentWeather(event){
+    event.preventDefault();
+}
