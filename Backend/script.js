bee3be04641e25a3cde8ce8130e463d95606d0dd
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-07 18:45:29 -0500

    awful day no real progress

--- Backend/script.js
+++ Backend/script.js
@@ -10,32 +10,60 @@ var curretnWind = $("#wind");
 var currentHumidity  = $("#humidity");
 var currentUv = $("#uvIndex");
 var pastCity = [];
+
+
+
+var previousCity = JSON.parse(localStorage.getItem("previousCity")) || pastCity;
+console.log(previousCity)
+previousCity.forEach(city => {
+    searchedList = $(`<li class="list-group-item list-group-item-secondary">${city.toUpperCase()}</li>`);
+    $("#search-history").append(searchedList);
+});
+
 //Checks to see if the city has previously been searched
-function find(p){
-    for(var i=0; i<pastCity.lengeth; i++) {
-        if(p.toUpperCase() === pastCity[i]){
-            return -1;
-        }
-    }
-    return 1;
-}
-//set up thr API key used to find data
-var APIkey = "104b3d87a3f27b63c86227e77149ab4c"
-//tkaes the user input adds it to the list and adds to the local, value not showing city
+// function find(){
+//     previousCity.forEach(city =>  {
+//         if(city.toUpperCase() === previousCity[i]){
+//             return -1;
+//         }
+//         return 1;
+//     }
+   
+// )}
+//takes the user input adds it to the list and adds to the local, value not showing city
 $("#search-button").on("click", function(event){
     event.preventDefault();
     var city = $("#citySearch").val().trim();
-    if(!pastCity.includes(city)) {
-        pastCity.push(city);
+        previousCity.push(city);
         searchedList = $(`<li class="list-group-item list-group-item-secondary">${city.toUpperCase()}</li>`);
         $("#search-history").append(searchedList);
-    }
-    localStorage.setItem("city", JSON.stringify(searchedList));
-    console.log(searchedList)
+        console.log(pastCity)
+    
+    localStorage.setItem("previousCity", JSON.stringify(previousCity));
+    console.log(city)
 })
 
+fetchWeather: (event) => {
+    var selectedCity = document.getElementById('citySearch').value;
+    var key = "104b3d87a3f27b63c86227e77149ab4c"
+    var lang = 'en'
+    var units = 'imperial'
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=${key}&lang=${lang}&units=${units}`;
+    fetch(url)
+    .then(resp=> console.log(resp))
+    //     if(!resp.ok) throw new Error(resp.statusText);
+    //     return resp.json();
+        
+    // })
+    // console.log(resp)
+    // .then(data=>{
+
+    // })
+    // .catch(console.err);
+    
 
 
+}
 
 //function to display the data for city input by user
 // function showCurrentWeather(event){
