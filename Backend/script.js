ee828fb260d8c262129a41e11864fac7afb8ee09
BryceBann <109399715+BryceBann@users.noreply.github.com>
2022-09-08 09:42:44 -0500

    possibly found issue

--- Backend/script.js
+++ Backend/script.js
@@ -1,5 +1,5 @@
+//making variables through html
 var city = "";
-
 var searchedList = $("#search-history");
 var searchCity = $("#citySearch");
 var searchBtn = $("#search-button");
@@ -12,33 +12,37 @@ var currentUv = $("#uvIndex");
 var pastCity = [];
 
 
-
+//if the local storage has cities add to array for use and display if empty us declaried array
 var previousCity = JSON.parse(localStorage.getItem("previousCity")) || pastCity;
 console.log(previousCity)
 previousCity.forEach(city => {
     searchedList = $(`<li class="list-group-item list-group-item-secondary">${city.toUpperCase()}</li>`);
     $("#search-history").append(searchedList);
 });
-
+//on the click add the user input to the array and add the city to list to display
 $("#search-button").on("click", function(event){
     event.preventDefault();
       city = $("#citySearch").val().trim();
         previousCity.push(city);
         searchedList = $(`<li class="list-group-item list-group-item-secondary">${city.toUpperCase()}</li>`);
         $("#search-history").append(searchedList);
-        console.log(pastCity)
-    
+        console.log(pastCity);
     localStorage.setItem("previousCity", JSON.stringify(previousCity));
-    console.log(city)
+    console.log(city);
+    
 })
+currentWeather(city);
 
-console.log(city)
+//fetching the weather with the api using city name and key 
+function currentWeather(city) {
+    var key = "104b3d87a3f27b63c86227e77149ab4c"
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${previousCity[1]}&appid=${key}&unit=imperial`)
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+    })
+  
+    }
 
-function currentWeather() {
-var key = "104b3d87a3f27b63c86227e77149ab4c"
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-.then(response => response.json())
-.then(data =>{
-    console.log(data)
-})
-}
+//uv index api call seperatley from weather
+// https://api.openweathermap.org/data/2.5/uvi?q=${city}&appid=${key}
